6261856d2013ece5e065fd124853a705ab0d4785
Praveen Balijepalli <vnpraveenb@gmail.com>
2017-11-28 21:39:33 +0530

    Initial Commit

---
+++ help.js
@@ -0,0 +1,25 @@
+//function to change the content of the <p> element with id=helpElement when clicked on any of the textfields of input tag
+function helpNote(element){
+    var helpId = element.id; //Storing the value of id of element calling helpNote() function
+    
+    //This function is a closure which changes the content of helpELement tag's innerHTML if id of the clicked element matches a given string
+ function changeText(){
+         
+        if( helpId == "username")
+        {
+            document.getElementById("helpElement").innerHTML = "Please input the username";
+        }
+        else if( helpId == "age")
+        {
+            document.getElementById("helpElement").innerHTML = "Please input the age";
+        } 
+        else if( helpId == "city")
+        {
+            document.getElementById("helpElement").innerHTML = "Please input the city";
+        }         
+    }
+    changeText();    
+}
+
+
+ 
